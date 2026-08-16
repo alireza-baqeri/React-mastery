9b3fe65cbc17591793926c16c214b63e93295a64
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-08-16 12:12:38 +0330

    Initial commit

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <div className=" h-30 text-4xl text-center bg-yellow-500 text-green-500 text-shadow-red-300">hey</div>;
+}
+
+export default Header
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div className="text-2xl">
+            hey
+        </div>
+    )
+}
+
+export default HomePage
